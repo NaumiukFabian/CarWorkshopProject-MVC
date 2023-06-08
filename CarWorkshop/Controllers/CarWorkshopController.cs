5b8561f7e195f7b42c05239d0467afdd5253aa6e
Fabian Naumiuk <fabian.naumiuk@gmail.com>
2023-06-08 12:10:32 +0200

    AddedInjectionsBeetwenProjects

--- CarWorkshop/Controllers/CarWorkshopController.cs
+++ CarWorkshop/Controllers/CarWorkshopController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarWorkshop.Application.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarWorkshop.Controllers
 {
     public class CarWorkshopController : Controller
     {
-        [HttpPost]
-        public IActionResult Creat(Domain.Entities.CarWorkshop carWorkshop)
-        {
+        private readonly ICarWorkshopService _carWorkshopService;
 
+        public CarWorkshopController(ICarWorkshopService carWorkshopService)
+        {
+            _carWorkshopService = carWorkshopService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Creat(Domain.Entities.CarWorkshop carWorkshop)
+        {
+            await _carWorkshopService.Creat(carWorkshop);
+            return RedirectToAction(nameof(Creat));
         }
     }
 }
